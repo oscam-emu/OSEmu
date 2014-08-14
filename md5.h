e3f4e504b2e90c19b8c6d0dec84ae8d9b2c9a576
M <jahntheman@users.noreply.github.com>
2014-08-14 15:13:16 +0200

    synced crypto code with oscam.emu

--- md5.h
+++ md5.h
@@ -1,4 +1,12 @@
+#ifndef _CSCRYPT_MD5_H
+#define _CSCRYPT_MD5_H
+
+#if defined(WITH_SSL) || defined(WITH_LIBCRYPTO)
+#  include <openssl/md5.h>
+#else
 #define MD5_DIGEST_LENGTH 16
+
+unsigned char *MD5(const unsigned char *input, unsigned long len, unsigned char *output_hash);
 
 typedef struct MD5Context {
 	uint32_t buf[4];
@@ -6,9 +14,9 @@ typedef struct MD5Context {
 	unsigned char in[64];
 } MD5_CTX;
 
-unsigned char *MD5(const unsigned char *input, unsigned long len, unsigned char *output_hash);
-char * __md5_crypt(const char *text_pass, const char *salt, char *crypted_passwd);
 void MD5_Init(MD5_CTX *ctx);
 void MD5_Update(MD5_CTX *ctx, const unsigned char *buf, unsigned int len);
 void MD5_Final(unsigned char digest[MD5_DIGEST_LENGTH], MD5_CTX *ctx);
-
+#endif
+char *__md5_crypt(const char *text_pass, const char *salt, char *crypted_passwd);
+#endif
