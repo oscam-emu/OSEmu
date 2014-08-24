e24f9d11b3b82aab2e6e9615baf4cb5bf4c73013
M <jahntheman@users.noreply.github.com>
2014-08-24 13:11:33 +0200

    switched to modern integer types

--- emulator.h
+++ emulator.h
@@ -1,4 +1,4 @@
-char ProcessECM(uint16_t CAID, const unsigned char *ecm, unsigned char *dw);
+int8_t ProcessECM(uint16_t CAID, const uint8_t *ecm, uint8_t *dw);
 void read_emu_keyfile(char *path);
 #ifndef __APPLE__
 void read_emu_keymemory(void);
