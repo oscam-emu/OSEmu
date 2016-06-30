fe9f56ca69df23a054fe1a54f8259bea6fb16544
jahntheman <jahntheman@users.noreply.github.com>
2016-06-30 15:38:11 +0200

    Updated header

--- module-emulator-osemu.c
+++ module-emulator-osemu.c
@@ -362,7 +362,7 @@ int32_t SetKey(char identifier, uint32_t provider, char *keyName, uint8_t *orgKe
 	return 1;
 }
 
-static int32_t FindKey(char identifier, uint32_t provider, uint32_t providerIgnoreMask, const char *keyName, uint8_t *key, 
+int32_t FindKey(char identifier, uint32_t provider, uint32_t providerIgnoreMask, const char *keyName, uint8_t *key, 
 										uint32_t maxKeyLength, uint8_t isCriticalKey, uint32_t keyRef, uint8_t matchLength, uint32_t *getProvider)
 {
 	uint32_t i;
